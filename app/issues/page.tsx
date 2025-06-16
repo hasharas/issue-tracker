8d56ed2b5ddc24e9204cdf8b4ce4bb70f477957a
hasharas <hasharamsankalpam@gmail.com>
2025-06-17 00:34:56 +0530

    Create:issues page create

---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IssuesPage = () => {
+      return (
+            <div>
+                  hellow issues
+            </div>
+      );
+}
+
+export default IssuesPage;
